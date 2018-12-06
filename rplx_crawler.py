cd072e0bbd44c4010075511ff7e4db2ed378ee05
alexandermuehle <alexander.muhle@gmail.com>
2018-12-06 17:45:18 +0100

    graceful shutdown

--- rplx_crawler.py
+++ rplx_crawler.py
@@ -1,7 +1,12 @@
-from server import Endpoint, PingNode, PingServer
-from secp256k1 import PrivateKey
+import logging
 import queue
 import sys
+import threading
+from server import Endpoint, PingNode, PingServer
+from secp256k1 import PrivateKey
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 
 #read threadcount from args
 threadcount = 1
@@ -17,19 +22,35 @@ with open("priv_key", 'w') as f:
 q = queue.Queue()
 qset = queue.Queue()
 q.put(Endpoint(u'52.16.188.185', 30303, 30303, bytes.fromhex("a979fb575495b8d6db44f750317d0f4622bf4c2aa3365d6af7c284339968eef29b69ad0dce72a4d8db5ebb4968de0e3bec910127f134779fbcb0cb6d3331163c"))) #geth
+q.put(Endpoint(u'13.93.211.84', 30303, 30303, bytes.fromhex("3f1d12044546b76342d59d4a05532c14b85aa669704bfe1f864fe079415aa2c02d743e03218e57a33fb94523adb54032871a6c51b2cc5514cb7c7e35b3ed0a99")))
+q.put(Endpoint(u'191.235.84.50', 30303, 30303, bytes.fromhex("78de8a0916848093c73790ead81d1928bec737d565119932b98c6b100d944b7a95e94f847f689fc723399d2e31129d182f7ef3863f2b4c820abbf3ab2722344d")))
+q.put(Endpoint(u'13.75.154.138', 30303, 30303, bytes.fromhex("158f8aab45f6d19c6cbf4a089c2670541a8da11978a2f90dbf6a502a4a3bab80d288afdbeb7ec0ef6d92de563767f3b1ea9e8e334ca711e9f8e2df5a0385e8e6")))
 q.put(Endpoint(u'174.112.32.157', 30303, 30303, bytes.fromhex("e809c4a2fec7daed400e5e28564e23693b23b2cc5a019b612505631bbe7b9ccf709c1796d2a3d29ef2b045f210caf51e3c4f5b6d3587d43ad5d6397526fa6179"))) #parity
 q.put(Endpoint(u'144.76.62.101', 30303, 30303, bytes.fromhex("2676755dd8477ad3beea32b4e5a144fa10444b70dfa3e05effb0fdfa75683ebd4f75709e1f8126cb5317c5a35cae823d503744e790a3a038ae5dd60f51ee9101"))) #pyethapp
+#fill queue set also
+for point in list(q.queue):
+	qset.put(point)
 
 out = queue.Queue()
 
 #start threads for discovery
+threads = []
+running = threading.Event()
+running.set()
 server = PingServer(Endpoint(u'127.0.0.1', 30303, 30303, k.serialize()))
 for x in range(threadcount):
-	server.discover(q, qset, out, x).start()
-
+	t = threading.Thread(target = server.discover, args = (q, qset, out, x, running))
+	t.start()
+	threads.append(t)
 
 with open("crawl_result.txt", 'w') as f:
-	while True:
-		message = out.get()
-		f.write(message + "\n")
-	
+	try:	
+		while True:
+			message = out.get()
+			f.write(message + "\n")
+	except KeyboardInterrupt:
+		logger.info("Shutting down writer")
+		running.clear()
+		logging.info("Shutting down Discovery")
+		for thread in threads:
+			thread.join()
--- server.py
+++ server.py
@@ -15,6 +15,7 @@ from ipaddress import ip_address
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
+logging.basicConfig(format="%(threadName)s:%(message)s")
 
 #keccak hashing
 def keccak256(s):
@@ -100,157 +101,154 @@ class PingServer(object):
 		#hash + signature + packet_type + packet_data
 		return payload_hash + payload
 	
-	def discover(self, q, qset, out, count):
+	def discover(self, q, qset, out, count, running):
+
+		#socket
 		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 		sock.bind(('0.0.0.0', self.endpoint.udpPort+count))
 		sock.settimeout(5)
-		self.their_endpoint = q.get()
 
-		def conversation():
-
-			def request_neighbour_lsb(bucket):
-				#generate nodeID to discover specified bucket
-				hashedID = keccak256(self.their_endpoint.nodeID)
-				match = False
-				mask = 1
-				while match is False:
-					generatedID = bytes(random.randint(0,255) for _ in range(64))
-					genHashed = keccak256(generatedID)
-					if bucket == 0:
-						if (genHashed[0] ^ hashedID[0]) & mask == mask:
+		def request_neighbour_lsb(bucket):
+			#generate nodeID to discover specified bucket
+			hashedID = keccak256(self.their_endpoint.nodeID)
+			match = False
+			mask = 1
+			while match is False:
+				generatedID = bytes(random.randint(0,255) for _ in range(64))
+				genHashed = keccak256(generatedID)
+				if bucket == 0:
+					if (genHashed[0] ^ hashedID[0]) & mask == mask:
+						match = True
+					else:
+						match = False
+				else:
+					#get the bytes in which the relevant bits are and iterate over them
+					for x in range(int(bucket/8)+1):
+						#make mask according to position in byte array (if last relevant byte get specific bit mask)
+						if x == int(bucket/8):
+							mask = pow(2, bucket%8)-1
+						else:
+							mask = 255
+						#actual comparing
+						if ~(genHashed[x] ^ hashedID[x]) & mask == mask:
 							match = True
 						else:
 							match = False
+							break
+
+			#send request
+			find_neighbour = NeighbourNode(generatedID)
+			message = self.wrap_packet(find_neighbour)
+			logging.debug("sending find_node")
+			sock.sendto(message, (self.their_endpoint.address.exploded, self.their_endpoint.udpPort))
+
+		def request_neighbour_msb(bucket):
+			#generate nodeID to discover specified bucket
+			hashedID = keccak256(self.their_endpoint.nodeID)
+			match = False
+			mask = 1 << 7
+			while match is False:
+				generatedID = bytes(random.randint(0,255) for _ in range(64))
+				genHashed = keccak256(generatedID)
+				if bucket == 0:
+					if (genHashed[31] ^ hashedID[31]) & mask == mask:
+						match = True
 					else:
-						#get the bytes in which the relevant bits are and iterate over them
-						for x in range(int(bucket/8)+1):
-							#make mask according to position in byte array (if last relevant byte get specific bit mask)
-							if x == int(bucket/8):
-								mask = pow(2, bucket%8)-1
-							else:
-								mask = 255
-							#actual comparing
-							if ~(genHashed[x] ^ hashedID[x]) & mask == mask:
-								match = True
-							else:
-								match = False
-								break
-
-				#send request
-				find_neighbour = NeighbourNode(generatedID)
-				message = self.wrap_packet(find_neighbour)
-				logging.debug("sending find_node")
-				sock.sendto(message, (self.their_endpoint.address.exploded, self.their_endpoint.udpPort))
-
-			def request_neighbour_msb(bucket):
-				#generate nodeID to discover specified bucket
-				hashedID = keccak256(self.their_endpoint.nodeID)
-				match = False
-				mask = 1 << 7
-				while match is False:
-					generatedID = bytes(random.randint(0,255) for _ in range(64))
-					genHashed = keccak256(generatedID)
-					if bucket == 0:
-						if (genHashed[31] ^ hashedID[31]) & mask == mask:
+						match = False
+				else:
+					#get the bytes in which the relevant bits are and iterate over them
+					for x in range(int(bucket/8)+1):
+						#make mask according to position in byte array (if last relevant byte get specific bit mask)
+						if x == int(bucket/8):
+							mask = pow(2, bucket%8)-1
+							mask = mask << (8-bucket%8)
+						else:
+							mask = 255
+						#actual comparing
+						if ~(genHashed[31-x] ^ hashedID[31-x]) & mask == mask:
 							match = True
 						else:
 							match = False
-					else:
-						#get the bytes in which the relevant bits are and iterate over them
-						for x in range(int(bucket/8)+1):
-							#make mask according to position in byte array (if last relevant byte get specific bit mask)
-							if x == int(bucket/8):
-								mask = pow(2, bucket%8)-1
-								mask = mask << (8-bucket%8)
-							else:
-								mask = 255
-							#actual comparing
-							if ~(genHashed[31-x] ^ hashedID[31-x]) & mask == mask:
-								match = True
-							else:
-								match = False
-								break
+							break
 
-				#send request
-				find_neighbour = NeighbourNode(generatedID)
-				message = self.wrap_packet(find_neighbour)
-				logging.debug("sending find_node")
-				sock.sendto(message, (self.their_endpoint.address.exploded, self.their_endpoint.udpPort))
+			#send request
+			find_neighbour = NeighbourNode(generatedID)
+			message = self.wrap_packet(find_neighbour)
+			logging.debug("sending find_node")
+			sock.sendto(message, (self.their_endpoint.address.exploded, self.their_endpoint.udpPort))
 
 
-			def decode_worker(q, data, workerOut):
-				nodes = rlp.decode(data[98:])[0] #[0] nodes [1] expiration
-				for node in nodes:
-					ip = ip_address(node[0])
-					if len(node[1]) == 2:	
-						udp_port = struct.unpack(">H", node[1])
-					if len(node[2]) == 2:
-						tcp_port = struct.unpack(">H", node[2])
-					else:
-						tcp_port = (0, ">I")
-					nodeID = node[3]
-					neighbour = Endpoint(str(ip), udp_port[0], tcp_port[0], nodeID)
-					logging.debug("Neighbour: " + neighbour.serialize())
-					if neighbour not in list(qset.queue):
-						q.put(neighbour)
-						qset.put(neighbour)
-						
-					workerOut.put(neighbour)
+		def decode_worker(q, data, workerOut):
+			nodes = rlp.decode(data[98:])[0] #[0] nodes [1] expiration
+			for node in nodes:
+				ip = ip_address(node[0])
+				if len(node[1]) == 2:	
+					udp_port = struct.unpack(">H", node[1])
+				if len(node[2]) == 2:
+					tcp_port = struct.unpack(">H", node[2])
+				else:
+					tcp_port = (0, ">I")
+				nodeID = node[3]
+				neighbour = Endpoint(str(ip), udp_port[0], tcp_port[0], nodeID)
+				logging.debug("Neighbour: " + neighbour.serialize())
+				if neighbour not in list(qset.queue):
+					q.put(neighbour)
+					qset.put(neighbour)
+					
+				workerOut.put(neighbour)
 
-			while True:
-				#start ping pong
-				ping = PingNode(self.endpoint, self.their_endpoint)
-				message = self.wrap_packet(ping)
-				logging.info("sending ping to " + self.their_endpoint.address.exploded)
-				sock.sendto(message, (self.their_endpoint.address.exploded, self.their_endpoint.udpPort))
-				serializeOut = self.their_endpoint.serialize()
-				greeted = False
-				while not greeted:
-					try:
-						data, addr = sock.recvfrom(1280)
-						if data[97] == 1:
-							logging.info("received ping from " + addr[0])	
-							pinger = Endpoint(addr[0], addr[1], addr[1], b'')
-							pong = PongNode(pinger, data[:32])
-							message = self.wrap_packet(pong)
-							logging.info("sending pong to " + str(pinger.address))
-							sock.sendto(message, (pinger.address.exploded, pinger.udpPort))	
-							greeted = True
-						if data[97] == 2:
-							logging.info("received pong from " + addr[0])
-					except socket.timeout:
-						serializeOut += ", no greeting"
-						break
-				#discover top 14 buckets of a neigbour (finding targets for all buckets would be hard/impossible)
-				if greeted:
-					for bucket in range(14):
-						#discover a bucket
-						request_neighbour_msb(bucket)
-						counter = 0
-						workerOut = queue.Queue()
-						workers = []
-						while counter < 2:
-							try:
-								data, addr = sock.recvfrom(1280)
-								if data[97] == 4:
-									#get up to 16 neighbours and add them to the q (12 neighbours per packet)
-									workers.append(Thread(target = decode_worker, args = (q, data, workerOut)))
-									workers[counter].start()
-									logging.debug("received neighbours from " + addr[0])
-									counter += 1
-							#timeout because we received all neighbours available (less than 16) or didnt receive ping 
-							except socket.timeout:
-								break
-						for worker in workers:
-							worker.join()
-						serializeOut += ", \"["
-						for item in list(workerOut.queue):
-							serializeOut += "[" + item.serialize() + "], "
-						if serializeOut[-2] == ',':
-							serializeOut = serializeOut[:-2]
-						serializeOut += "]\""
-				logging.debug(serializeOut)
-				out.put(serializeOut)
-				self.their_endpoint = q.get()
-
-		return threading.Thread(target = conversation)
+		while running.is_set():
+			#start ping pong
+			self.their_endpoint = q.get()
+			ping = PingNode(self.endpoint, self.their_endpoint)
+			message = self.wrap_packet(ping)
+			logging.info("sending ping to " + self.their_endpoint.address.exploded)
+			sock.sendto(message, (self.their_endpoint.address.exploded, self.their_endpoint.udpPort))
+			serializeOut = self.their_endpoint.serialize()
+			greeted = False
+			while not greeted:
+				try:
+					data, addr = sock.recvfrom(1280)
+					if data[97] == 1:
+						logging.info("received ping from " + addr[0])	
+						pinger = Endpoint(addr[0], addr[1], addr[1], b'')
+						pong = PongNode(pinger, data[:32])
+						message = self.wrap_packet(pong)
+						logging.info("sending pong to " + str(pinger.address))
+						sock.sendto(message, (pinger.address.exploded, pinger.udpPort))	
+						greeted = True
+					if data[97] == 2:
+						logging.info("received pong from " + addr[0])
+				except socket.timeout:
+					serializeOut += ", no greeting"
+					break
+			#discover top 14 buckets of a neigbour (finding targets for all buckets would be hard/impossible)
+			if greeted:
+				for bucket in range(14):
+					#discover a bucket
+					request_neighbour_msb(bucket)
+					counter = 0
+					workerOut = queue.Queue()
+					workers = []
+					while counter < 2:
+						try:
+							data, addr = sock.recvfrom(1280)
+							if data[97] == 4:
+								#get up to 16 neighbours and add them to the q (12 neighbours per packet)
+								workers.append(Thread(target = decode_worker, args = (q, data, workerOut)))
+								workers[counter].start()
+								logging.debug("received neighbours from " + addr[0])
+								counter += 1
+						#timeout because we received all neighbours available (less than 16) or didnt receive ping 
+						except socket.timeout:
+							break
+					for worker in workers:
+						worker.join()
+					serializeOut += ", \"["
+					for item in list(workerOut.queue):
+						serializeOut += "[" + item.serialize() + "], "
+					if serializeOut[-2] == ',':
+						serializeOut = serializeOut[:-2]
+					serializeOut += "]\""
+			logging.debug(serializeOut)
+			out.put(serializeOut)
